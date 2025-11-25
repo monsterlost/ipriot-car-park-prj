1fb5ecd3aea5419975e7b9946a5f62fb4b95d395
20149130 <20149130@tafe.wa.edu.au>
2025-11-25 16:15:56 +0800

    feat: add carpark constructor

--- src/car_park.py
+++ src/car_park.py
@@ -1,2 +1,9 @@
 class CarPark:
-    pass
+    def __init__(self, location, capacity, plates=None, displays=None):
+        self.location = location
+        self.capacity = capacity
+        self.plates = plates or [] #list - holds references to license plates represented as strings
+        self.displays = displays or [] #list - holds references to instances of displays
+
+    def __str__(self):
+        return f"Car park at {self.location}, with {self.capacity} bays"
