a98e0f07ceb68bb1d20dc6b6187b741dcc77f92d
20149130 <20149130@tafe.wa.edu.au>
2025-11-25 16:39:33 +0800

    feat: implement display and sensor constructors

--- src/sensor.py
+++ src/sensor.py
@@ -1,2 +1,14 @@
 class Sensor:
+    def __init__(self, id, is_active, car_park):
+        self.id = id
+        self.is_active = is_active
+        self.car_park = car_park
+
+    def __str__(self):
+        return f"Sensor {id} is {self.is_active}"
+
+class EntrySensor(Sensor):
+    pass
+
+class ExitSensor(Sensor):
     pass
