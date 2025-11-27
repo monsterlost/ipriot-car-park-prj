3e629fc2b8ae26e27debc9d5a891d8a5066259ae
20149130 <20149130@tafe.wa.edu.au>
2025-11-27 14:21:04 +0800

    test: add unit tests for log in car park class

--- src/car_park.py
+++ src/car_park.py
@@ -1,15 +1,17 @@
 from sensor import Sensor
 from display import Display
 from pathlib import Path
+from datetime import datetime
 
 
 class CarPark:
-    def __init__(self, location, capacity, plates=None, displays=None, log_file = "log.txt"):
+    def __init__(self, location, capacity, plates=None, displays=None, log_file=Path("log.txt")):
         self.location = location
         self.capacity = capacity
         self.plates = plates or [] #list - holds references to license plates represented as strings
         self.displays = displays or [] #list - holds references to instances of displays
-        self.log_file = Path(log_file)
+        self.log_file = log_file if isinstance(log_file, Path) else Path(log_file)
+        self.log_file.touch(exist_ok=True)
 
     def __str__(self):
         return f"Car park at {self.location}, with {self.capacity} bays"
@@ -32,12 +34,18 @@ class CarPark:
     def add_car(self, plate):
         self.plates.append(plate)
         self.update_displays()
+        self._log_car_activity(plate, "entered")
 
     def remove_car(self, plate):
         self.plates.remove(plate)
         self.update_displays()
+        self._log_car_activity(plate, "exited")
 
     def update_displays(self):
         data = {"Available Bays": self.available_bays, "Temperature": 20} #todo: temperature variable
         for display in self.displays:
             display.update(data)
+
+    def _log_car_activity(self, plate, action):
+        with self.log_file.open("a") as f:
+            f.write(f"{plate} {action} at {datetime.now():%Y-%m-%d %H:%M:%S}\n")
