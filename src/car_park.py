26eaaf43241b7bf9ceaa88bac30aa5a711548578
20149130 <20149130@tafe.wa.edu.au>
2025-11-27 15:15:12 +0800

    feat: add method to store configuration

--- src/car_park.py
+++ src/car_park.py
@@ -2,16 +2,19 @@ from sensor import Sensor
 from display import Display
 from pathlib import Path
 from datetime import datetime
+import json
 
 
 class CarPark:
-    def __init__(self, location, capacity, plates=None, displays=None, log_file=Path("log.txt")):
+    def __init__(self, location, capacity, plates=None, displays=None, log_file=Path("log.txt"), config_file=Path("config.json")):
         self.location = location
         self.capacity = capacity
         self.plates = plates or [] #list - holds references to license plates represented as strings
         self.displays = displays or [] #list - holds references to instances of displays
         self.log_file = log_file if isinstance(log_file, Path) else Path(log_file)
         self.log_file.touch(exist_ok=True)
+        self.config_file = config_file if isinstance(config_file, Path) else Path(config_file)
+        self.config_file.touch(exist_ok=True)
 
     def __str__(self):
         return f"Car park at {self.location}, with {self.capacity} bays"
@@ -42,10 +45,23 @@ class CarPark:
         self._log_car_activity(plate, "exited")
 
     def update_displays(self):
-        data = {"Available Bays": self.available_bays, "Temperature": 20} #todo: temperature variable
+        data = {"Available Bays": self.available_bays, "Temperature": 20}
         for display in self.displays:
             display.update(data)
 
     def _log_car_activity(self, plate, action):
         with self.log_file.open("a") as f:
             f.write(f"{plate} {action} at {datetime.now():%Y-%m-%d %H:%M:%S}\n")
+
+    def write_config(self):
+        with open(self.config_file, "w") as f:
+            json.dump({"location": self.location,
+                       "capacity": self.capacity,
+                       "log_file": str(self.log_file)}, f)
+
+    @classmethod
+    def from_config(cls, config_file=Path("config.json")):
+        config_file = config_file if isinstance(config_file, Path) else Path(config_file)
+        with config_file.open() as f:
+            config = json.load(f)
+        return cls(config["location"], config["capacity"], log_file=config["log_file"])
